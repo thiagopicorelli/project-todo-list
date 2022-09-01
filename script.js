bda2ccee6a39d8f55118f7543d3dcfc35cccc950
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-01 16:37:06 -0300

    Adiciona função de selecionar item da lista

--- script.js
+++ script.js
@@ -2,10 +2,26 @@ const addInput = document.getElementById('texto-tarefa');
 const addButton = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 
+let listElements;
+let listCount = 0;
+let selected = 0;
+
+function selecionaItem(event) {
+  listElements[selected].style.backgroundColor = 'transparent';
+  listElements[event.target.pos].style.backgroundColor = 'rgb(128, 128, 128)';
+  selected = event.target.getAttribute('pos');
+}
+
 function criarTarefa() {
   const tarefa = document.createElement('li');
   tarefa.innerHTML = addInput.value;
+  tarefa.setAttribute('pos', listCount);
+  tarefa.addEventListener('click', selecionaItem);
+
   list.appendChild(tarefa);
+  listElements = document.getElementsByTagName('li');
+  listCount += 1;
+
   addInput.value = '';
 }
 
